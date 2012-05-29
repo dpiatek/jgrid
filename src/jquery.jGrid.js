39d6989cb847726d80e6cbec15523f66b48e8404
Dominik Piatek <dominik@madebyredstar.com>
2012-05-29 08:45:45 +0100

    Initialized grunt default

---
+++ src/jquery.jGrid.js
@@ -0,0 +1,28 @@
+/*
+ * jquery.jGrid
+ * https://github.com/dpiatek/jGrid
+ *
+ * Copyright (c) 2012 Dominik Piatek
+ * Licensed under the MIT, GPL licenses.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
